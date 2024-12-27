5f54b4202233cd1ae07ea2986f145469a62a6c3e
CometB <126880018+CometB@users.noreply.github.com>
2024-12-27 12:30:55 +0700

    .

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+long long fibo[100] = {};
+
+long long fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long fibonacci(int x){
+    if(x == 0) return 0;
+    if(x == 1) return 1;
+    if(fibo[x]) return fibo[x];
+    fibo[x] = fibonacci(x-1) + fibonacci(x-2);
+    return fibo[x];
 }
